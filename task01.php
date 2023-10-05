a2729294ef9f2c674326294080ca90586fa07529
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-05 12:50:30 +0200

    change task01

--- task01.php
+++ task01.php
@@ -5,7 +5,7 @@ function display_menu() {
         <ul>
             <li><a href="home.php">Home</a></li>
             <li><a href="product.php">Products</a></li>
-            <li><a href="about.php">About</a></li>
+            <li><a href="about.php">About Us</a></li>
             <li><a href="contact.php">Contact</a></li>  
         </ul>
     
