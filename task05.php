6ddc8a00098aa2eded9387a5fcf8e63935ff2182
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-05 14:33:15 +0200

    add format for curriculum

--- task05.php
+++ task05.php
@@ -6,6 +6,14 @@ function form_is_submitted() {
 
 function whoami()
 {
+
+    $format = [
+        "msc" => "MSc Pro",
+        "pge" => "PGE (Programme Grande Ecole)",
+        "coding" => "Coding Academy",
+        "wac" => "Web@cademie"
+    ];
+
     $name = isset($_POST["name"]) && !empty($_POST['name']) ? $_POST['name'] : null;
     $age = isset($_POST["age"]) && !empty($_POST['age']) && is_numeric($_POST['age']) ? $_POST['age'] : null;
 
@@ -24,7 +32,7 @@ function whoami()
     }
 
     if ($curriculum !== '') {
-        $res .= " I'm a student of $curriculum.";
+        $res .= " I'm a student of $format[$curriculum].";
     }
 
     echo $res;
