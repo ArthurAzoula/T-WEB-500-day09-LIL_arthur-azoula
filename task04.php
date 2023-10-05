28255a380402d01baba9baf750e02d1a4e3be06a
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-05 12:06:59 +0200

    add task04

---
+++ task04.php
@@ -0,0 +1,18 @@
+<?php
+
+function whoami() {
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+
+    $isAgeValid = isset($age) && is_numeric($age);
+
+    if(isset($name) && $isAgeValid) {
+        echo "Hi, my name is $name and I'm $age years old.";
+    } else if(isset($name)) {
+        echo "Hi, my name is $name."; 
+    } else if($isAgeValid) {
+        echo "Hi, I have no name and I'm $age years old.";
+    } else {
+        echo "Hi, i'm anonymous.";
+    }
+}
